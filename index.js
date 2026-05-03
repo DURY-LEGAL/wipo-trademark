02a59fb68f41f8938c6bdc2caf1576e2e7c05ed4
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:41:24 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// wipo-trademark - WIPO trademark tools - Madrid Protocol international trademark registration. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
